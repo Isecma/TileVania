bfb5eb937e78f95833d20bd11e539059cca7eb3d
Ismael Echegaray <ismael.echegarayms@hotmail.com>
2022-10-01 22:58:58 -0500

    Escalera
    -Se agregó la lógica y animación para el uso de las escaleras

--- TileVania/Assets/_Scripts/PlayerMovement.cs
+++ TileVania/Assets/_Scripts/PlayerMovement.cs
@@ -1,19 +1,24 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float runSpeed = 1;
-    [SerializeField] float jumpSpeed = 1;
-    
+    [SerializeField] float runSpeed;
+    [SerializeField] float jumpSpeed;
+    [SerializeField] float climbSpeed;
+        
     Vector2 moveInput;
     Rigidbody2D myRigidbody;
     Animator animator;
     int groundMask;
+    int ladderMask;
+    float defaultGravity;
+
 
     int numberOfJumps = 2;
     
@@ -23,17 +28,21 @@ public class PlayerMovement : MonoBehaviour
         myRigidbody = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         groundMask = LayerMask.GetMask("Ground");
+        ladderMask = LayerMask.GetMask("Ladder");
+        defaultGravity = myRigidbody.gravityScale;
     }
 
 
     void Update()
     {
-        Run();
+        Move();
         FlipSprite();
+        ClimbLadder();
     }
 
-    void Run()
+    void Move()
     {
+        animator.StopPlayback();
         Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, myRigidbody.velocity.y);
         myRigidbody.velocity = playerVelocity;
 
@@ -45,6 +54,7 @@ public class PlayerMovement : MonoBehaviour
         {
             animator.SetBool("IsRunning", false);
         }
+
     }
 
     void OnMove(InputValue value)
@@ -53,6 +63,7 @@ public class PlayerMovement : MonoBehaviour
     }
     void OnJump(InputValue value)
     {
+
         if (value.isPressed && numberOfJumps > 0)
         {
             myRigidbody.velocity += new Vector2(0f, jumpSpeed);
@@ -66,7 +77,9 @@ public class PlayerMovement : MonoBehaviour
         {
             numberOfJumps = 2;
         } 
+
     }
+
 
     void FlipSprite()
     {
@@ -76,7 +89,39 @@ public class PlayerMovement : MonoBehaviour
         {
             transform.localScale = new Vector2(Mathf.Sign(moveInput.x), 1f);
         }
+    }
 
+
+    void ClimbLadder()
+    {
+        Vector2 playerClimbingVelocity;
+        if (!myRigidbody.IsTouchingLayers(ladderMask))
+        {
+            animator.SetBool("IsClimbing", false);
+            myRigidbody.gravityScale = defaultGravity;
+            return;
+        }
+
+        if (Mathf.Abs(moveInput.y) > 0f)
+        {
+            animator.StopPlayback();
+            animator.SetBool("IsClimbing", true);
+            playerClimbingVelocity = new Vector2(moveInput.x, (moveInput.y) * climbSpeed);
+            myRigidbody.velocity = playerClimbingVelocity;
+        }
+        else if (moveInput.y == 0f && animator.GetBool("IsClimbing"))
+        {
+            myRigidbody.gravityScale = 0f;
+            playerClimbingVelocity = new Vector2(moveInput.x, (moveInput.y) * climbSpeed);
+            myRigidbody.velocity = playerClimbingVelocity;
+            animator.StartPlayback();
+        }
+        else if (moveInput.y == 0f && !animator.GetBool("IsClimbing"))
+        {
+            myRigidbody.gravityScale = defaultGravity;
+            playerClimbingVelocity = new Vector2(moveInput.x * runSpeed, myRigidbody.velocity.y);
+            myRigidbody.velocity = playerClimbingVelocity;
+        }
         
     }
 }
