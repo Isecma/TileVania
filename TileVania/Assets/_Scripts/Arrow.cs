610a39bab51ccf08edc8cc688fc771fa7d3d5942
Ismael Echegaray <ismael.echegarayms@hotmail.com>
2022-10-04 19:04:01 -0500

    Creación de niveles
    -Se crearon dos niveles más

--- TileVania/Assets/_Scripts/Arrow.cs
+++ TileVania/Assets/_Scripts/Arrow.cs
@@ -6,21 +6,26 @@ public class Arrow : MonoBehaviour
 {
     [SerializeField] float arrowSpeed;
 
-    PlayerMovement playerMovement;
     float playerDirection;
+    bool hasStopped;
+    PlayerMovement playerMovement;
+    Animator animator;
     Rigidbody2D myRigidbody;
 
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
         playerMovement = FindObjectOfType<PlayerMovement>();
+        animator = GetComponent<Animator>();
         playerDirection = playerMovement.transform.localScale.x;
     }
 
     void Update()
     {
+        if (hasStopped) { return; }
         myRigidbody.velocity = new Vector2(playerDirection * arrowSpeed, 0f);   
         myRigidbody.transform.localScale = playerMovement.transform.localScale;
+        Invoke("DestroyArrow", 5);
     }
 
     void OnTriggerEnter2D(Collider2D collision)
@@ -28,7 +33,16 @@ public class Arrow : MonoBehaviour
         if (collision.tag == "Enemies")
         {
             Destroy(collision.gameObject);
+            DestroyArrow();
         }
+        hasStopped = true;
+        animator.StartPlayback();
+        myRigidbody.velocity = new Vector2(0f, 0f);
+        Invoke("DestroyArrow", 3);
+    }
+
+    void DestroyArrow()
+    {
         Destroy(gameObject);
     }
 }
